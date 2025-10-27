d4f0a864cddd96adf96e03942190cfbc3cec0bfa
Sita Vemuri <sitavemuri8@gmail.com>
2025-10-27 00:57:55 -0400

    overlay council district + correct labels

--- hamilton-heights/script.js
+++ hamilton-heights/script.js
@@ -14,3 +14,11 @@ regions
 const svg = document.querySelector("svg");
 const box = svg.getBBox();
 svg.setAttribute("viewBox", `${box.x} ${box.y - 1} ${box.width} ${box.height + 2}`);
+
+const rect = document.querySelector("rect");
+rect.setAttribute("x", box.x + 10);
+rect.setAttribute("y", box.y + 50);
+
+const legend = document.querySelector("#legend");
+legend.setAttribute("x", box.x + 185);
+legend.setAttribute("y", box.y + 70);
