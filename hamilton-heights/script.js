5143a5abd14b2209670642ff6567cc426e931681
Sita Vemuri <sitavemuri8@gmail.com>
2025-11-16 17:20:25 -0500

    clean up script.js

--- hamilton-heights/script.js
+++ hamilton-heights/script.js
@@ -1,20 +1,3 @@
-const regions = d3.selectAll(".neighborhood");
-const neighborhoodName = d3.select("#neighborhood-name");
-
-regions
-  .on("mouseover", function () {
-    d3.select(this).transition().duration(200).attr("fill", "#003865");
-    neighborhoodName.text(d3.select(this).attr("id"));
-  })
-  .on("mouseout", function () {
-    d3.select(this).transition().duration(200).attr("fill", "#ccc");
-    neighborhoodName.text("Hover over a neighborhood!");
-  });
-
-const svg = document.querySelector("svg");
-const box = svg.getBBox();
-svg.setAttribute("viewBox", `${box.x} ${box.y - 1} ${box.width} ${box.height + 2}`);
-
 const map = L.map("map", { minZoom: 13, maxZoom: 16 }).setView([40.82, -73.96], 13);
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution: "&copy; OpenStreetMap contributors",
